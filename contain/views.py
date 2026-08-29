73161157797074cd066ecef1daf079cd96f86881
unknown <mathewkarachu448@gmail.com>
2026-08-29 19:12:46 +0300

    views

--- contain/views.py
+++ contain/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
+def Home(request):
+    return render(request,"index.html")
+
+def Login(request):
+    return render(request,"login.html")
+
+def Signup(request):
+    return render(request, "signup.html")
 # Create your views here.
